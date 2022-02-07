b42e8e44666f605462ac6e13346ccd1a4103954f
ozkancondek <ozkncndek@gmail.com>
2022-02-07 16:42:52 +0100

    C2:Project structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { QuestionCard } from "./components/QuestionCard";
 
-function App() {
-  return <div className="App">quiz adwdapp</div>;
-}
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+  return (
+    <div className="App">
+      <h1>TypeScript-React Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="scor"> Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//define the types
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+export const QuestionCard = () => {
+  return <div>questioncard</div>;
+};
